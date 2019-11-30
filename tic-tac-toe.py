e8ba6f025ec3fe32a7d2962a8ee3b17c3ef82909
dragon <alexanderthomas93@gmail.com>
2019-11-30 19:12:10 +0000

    added start input funk

---
+++ tic-tac-toe.py
@@ -0,0 +1,24 @@
+#The guts
+
+
+def start_input():
+    starting_piece = input("hello player 1, would you like to be X or O? ")
+    return starting_piece
+
+
+def display_board():
+    board =
+
+
+def play_input():
+    pass
+
+
+def check_board():
+    pass
+
+
+def replay():
+    pass
+
+starting_piece = start_input()
