b39ab5b6b52675856fb7989db31f46cf416a71d7
green-drag0n <alexanderthomas93@gmail.com>
2019-12-14 18:18:43 -0500

    func skeleton

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -1,24 +1,29 @@
-#The guts
+def new_board():
+    board = [7,8,9,4,5,6,1,2,3]
+    return board
 
 
-def start_input():
-    starting_piece = input("hello player 1, would you like to be X or O? ")
-    return starting_piece
+def display_board(board):
+    print(f"{board[0]}|{board[1]}|{board[2]}")
+    print(f"{board[3]}|{board[4]}|{board[5]}")
+    print(f"{board[6]}|{board[7]}|{board[8]}")
+    return board
 
 
-def display_board():
-    board =
+def player_input(marker):
+    piece = input(f"player {marker} its your turn place your marker (X or O)")
+    return piece
 
 
-def play_input():
+def placement():
     pass
 
 
-def check_board():
+def board_check():
     pass
 
 
 def replay():
     pass
 
-starting_piece = start_input()
+display_board(new_board())
