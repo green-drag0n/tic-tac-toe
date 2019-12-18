682c6b5051a968a98375e82efd157628e34333a8
green-drag0n <alexanderthomas93@gmail.com>
2019-12-18 18:23:54 -0500

    established player turns and board editing/ returning

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -8,7 +8,7 @@ def intro():
 
 
 def new_board():
-    board = [7, 8, 9, 4, 5, 6, 1, 2, 3]
+    board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
     return board
 
 
@@ -19,20 +19,41 @@ def display_board(board):
     return board
 
 
-def player_input(name):
-    piece = input(f"{name}, its your turn place your marker (X or O)")
-    return piece
+def player_input(name, marker):
+    location = int(input(f"{name}, its your turn place your marker {marker}, where would you like it?"))
+    return location
 
 
-def placement():
-    pass
+def placement(board, num, x):
+    board[num-1] = x
+    return board
 
 
-def board_check():
-    pass
+
+def board_check(board):
+    print(board)
 
 
 def replay():
     pass
-player_one, player_two = intro()
-display_board(new_board())
+
+
+player_info = intro()
+board = new_board()
+
+
+turn = True
+no_winner = True
+while no_winner:
+    if turn:
+        display_board(board)
+        num = player_input(player_info[0], player_info[2])
+        board = placement(board, num, "X")
+        turn = False
+    else:
+        display_board(board)
+        num = player_input(player_info[1], player_info[3])
+        board = placement(board, num, "O")
+        turn = True
+    board_check(board)
+replay()
