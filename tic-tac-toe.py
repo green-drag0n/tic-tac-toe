9e9a4ec88aeaaa41064d09b98f9bcfac9da4c53c
green-drag0n <alexanderthomas93@gmail.com>
2019-12-21 11:23:12 -0500

    finished pretty much, tuning to do, to stop stupid people from throwing errors. the overwriting issue for next fix

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -30,30 +30,63 @@ def placement(board, num, x):
 
 
 
-def board_check(board):
-    print(board)
+def board_check(board, player_info):
+    if board[0] == board[3] == board[6] or board[1] == board[4] == board[7]:
+        if board[0] == player_info[2]:
+            print(f'{player_info[0]} you won')
+        else:
+            print(f'{player_info[1]} you won')
+        return False
+    elif board[2] == board[5] == board[8] or board[0] == board[1] == board[2]:
+        if board[0] == player_info[2]:
+            print(f'{player_info[0]} you won')
+        else:
+            print(f'{player_info[1]} you won')
+        return False
+    elif board[3] == board[4] == board[5] or board[6] == board[7] == board[8]:
+        if board[0] == player_info[2]:
+            print(f'{player_info[0]} you won')
+        else:
+            print(f'{player_info[1]} you won')
+        return False
+    elif board[0] == board[4] == board[8] or board[2] == board[4] == board[6]:
+        if board[0] == player_info[2]:
+            print(f'{player_info[0]} you won')
+        else:
+            print(f'{player_info[1]} you won')
+        return False
+    else:
+        return True
 
 
 def replay():
-    pass
+    choice = input("would you like to play again? y/n? >>> ")
+    return choice == 'y'
 
 
-player_info = intro()
-board = new_board()
+def the_game():
+    player_info = intro()
+    board = new_board()
 
 
-turn = True
-no_winner = True
-while no_winner:
-    if turn:
-        display_board(board)
-        num = player_input(player_info[0], player_info[2])
-        board = placement(board, num, "X")
-        turn = False
-    else:
-        display_board(board)
-        num = player_input(player_info[1], player_info[3])
-        board = placement(board, num, "O")
-        turn = True
-    board_check(board)
-replay()
+    turn = True
+    no_winner = True
+
+    while no_winner:
+        if turn:
+            display_board(board)
+            num = player_input(player_info[0], player_info[2])
+            board = placement(board, num, "X")
+            turn = False
+        else:
+            display_board(board)
+            num = player_input(player_info[1], player_info[3])
+            board = placement(board, num, "O")
+            turn = True
+        no_winner = board_check(board, player_info)
+    if replay():
+        the_game()
+
+
+if __name__ == '__main__':
+    the_game()
