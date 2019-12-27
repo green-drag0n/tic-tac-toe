764ff5f119a3b3b345851c2c1bf21b5a99eda917
green-drag0n <alexanderthomas93@gmail.com>
2019-12-27 11:21:45 -0500

    replay bug fix, incorrect player displayed as winner bug fix

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -39,26 +39,50 @@ def placement(board, num, x):
 
 
 def board_check(board, player_info):
-    if board[0] == board[3] == board[6] or board[1] == board[4] == board[7]:
+    if board[0] == board[3] == board[6]:
         if board[0] == player_info[2]:
             print(f'{player_info[0]} you won')
         else:
             print(f'{player_info[1]} you won')
         return False
-    elif board[2] == board[5] == board[8] or board[0] == board[1] == board[2]:
+    elif board[1] == board[4] == board[7]:
+        if board[1] == player_info[2]:
+            print(f'{player_info[0]} you won')
+        else:
+            print(f'{player_info[1]} you won')
+        return False
+    elif board[2] == board[5] == board[8]:
+        if board[2] == player_info[2]:
+            print(f'{player_info[0]} you won')
+        else:
+            print(f'{player_info[1]} you won')
+        return False
+    elif board[0] == board[1] == board[2]:
         if board[0] == player_info[2]:
             print(f'{player_info[0]} you won')
         else:
             print(f'{player_info[1]} you won')
         return False
-    elif board[3] == board[4] == board[5] or board[6] == board[7] == board[8]:
+    elif board[3] == board[4] == board[5]:
+        if board[3] == player_info[2]:
+            print(f'{player_info[0]} you won')
+        else:
+            print(f'{player_info[1]} you won')
+        return False
+    elif board[6] == board[7] == board[8]:
+        if board[6] == player_info[2]:
+            print(f'{player_info[0]} you won')
+        else:
+            print(f'{player_info[1]} you won')
+        return False
+    elif board[0] == board[4] == board[8]:
         if board[0] == player_info[2]:
             print(f'{player_info[0]} you won')
         else:
             print(f'{player_info[1]} you won')
         return False
-    elif board[0] == board[4] == board[8] or board[2] == board[4] == board[6]:
-        if board[0] == player_info[2]:
+    elif board[2] == board[4] == board[6]:
+        if board[2] == player_info[2]:
             print(f'{player_info[0]} you won')
         else:
             print(f'{player_info[1]} you won')
@@ -68,8 +92,14 @@ def board_check(board, player_info):
 
 
 def replay():
-    choice = input("would you like to play again? y/n? >>> ")
+    choice1 = ''
+    while choice1 not in ["y","n"]:
+
+        choice = input("would you like to play again? y/n? >>> ")
+        choice1 = choice.lower()
     return choice == 'y'
+
+
 
 
 def the_game():
